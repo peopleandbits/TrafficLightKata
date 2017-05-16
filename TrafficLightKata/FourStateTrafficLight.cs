85389dc0d76b08856bf48339d6b83f5784e4f37b
timo.toivonen <timo.toivonen@gfs-web.de>
2017-05-16 13:16:51 +0200

    comment

--- TrafficLightKata/FourStateTrafficLight.cs
+++ TrafficLightKata/FourStateTrafficLight.cs
@@ -4,6 +4,9 @@ using TrafficLightKata.Infra;
 
 namespace TrafficLightKata
 {
+    /// <summary>
+    /// Needs a comment...
+    /// </summary>
     public class FourStateTrafficLight : TrafficLightBase
     {
         public FourStateTrafficLight(StatePackage sp = null) : base(new FourStateLogic(), sp) { }
