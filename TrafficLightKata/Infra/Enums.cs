aeab5ecf87cd8feaf26144b775937a6135496a2b
timo.toivonen <timo.toivonen@gfs-web.de>
2017-05-17 17:30:58 +0200

    deleted stuff

--- TrafficLightKata/Infra/Enums.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TrafficLightKata.Infra
-{
-    // shared between ThreeStateTrafficLight and FourStateTrafficLight
-    public enum LightState { Red, RedYellow, Yellow, Green } 
-
-    public enum StateDirection { TowardsRed, TowardsGreen };
-}
